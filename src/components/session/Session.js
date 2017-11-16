11ebc3d94c8d2952ece3d19f635e8b7bbd5defad
michael-lowe-nz <mikkeyfluttie@gmail.com>
2017-11-17 02:09:38 +1300

    Add more styling and copy button

--- src/components/session/Session.js
+++ src/components/session/Session.js
@@ -3,6 +3,12 @@ import React from 'react'
 import Dishes from './Dishes'
 
 const Session = ({ state, match }) => {
+  function handleCopy () {
+    console.log('copying')
+    // `https://gitlab.cwp.govt.nz`.select();
+    document.execCommand("Copy")
+  }
+
   return (
     <div className="container">
       <div className="section">
@@ -11,10 +17,11 @@ const Session = ({ state, match }) => {
             <div className="column is-5">
               <h1 className="title is-4">{state.title}</h1>
               <h2 className="subtitle is-5">{state.location}</h2>
+              <a onClick={handleCopy} className="button is-outlined">Share Link</a>
             </div>
             <div className="column is-7">
               <div className="session-users">
-              <div className="user">
+              <div className="user is-active">
                 <p>Michael</p>
               </div>
               <div className="user">
