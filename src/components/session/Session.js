2abd21c5c244b1206450410bcb443055a0be0e2f
michael-lowe-nz <mikkeyfluttie@gmail.com>
2018-01-27 22:53:23 +1300

    Change Session to a class instead of function

--- src/components/session/Session.js
+++ src/components/session/Session.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import {push} from 'react-router-redux'
 import {bindActionCreators} from 'redux'
 import {connect} from 'react-redux'
 import {
@@ -11,67 +10,73 @@ import {
 
 import Dishes from './Dishes'
 
-const Session = ({title, dishes, addDish, count, setDishInput, dishInput}) => {
-
-  function handleChange (e) {
-    e.preventDefault()
-    setDishInput(e.target.value)
+class Session extends React.Component {
+  constructor (props) {
+    super(props)
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
-
-  function handleSubmit (e) {
+  handleChange (e) {
     e.preventDefault()
-    addDish({name: dishInput, votes: []})
-    setDishInput('')
+    this.props.setDishInput(e.target.value)
   }
-  return (
-  <div className="container">
-    <div className="section">
-      <div className="session">
-        <div className="columns is-multiline session-header">
-          <div className="column is-12">
-            <div className="session-users">
-              <div className="user is-active grow">
-                <p>M</p>
-              </div>
-              <div className="user grow">
-                <p>P</p>
-              </div>
-              <div className="user grow">
-                <p>T</p>
-              </div>
-            </div>
-          </div>
-          <div className="column is-5">
-            <h1 className="title is-4">{title}</h1>
-            <div className="columns">
-              <div className="column">
-                <a className="button is-outlined is-success grow">Share Link</a>
-              </div>
-              <div className="column is-8">
-                <form onSubmit={handleSubmit}>
-                <div className="field has-addons">
-                    <div className="control has-icons-left">
-                        <input value={dishInput} onChange={handleChange} name="dishInput" className="input" placeholder="Enter Dish"/>
-                        <span className="icon is-small is-left">
-                          <i className="fa fa-plus"></i>
-                        </span>
-                      </div>
-                      <div className="control">
-                        <a onClick={handleSubmit} className="button is-info">Add</a>
-                      </div>
+  handleSubmit (e) {
+    e.preventDefault()
+    this.props.addDish({name: this.props.dishInput, votes: []})
+    this.props.setDishInput('')
+  }
+  render() {
+    return (
+      <div className="container">
+        <div className="section">
+          <div className="session">
+            <div className="columns is-multiline session-header">
+              <div className="column is-12">
+                <div className="session-users">
+                  <div className="user is-active grow">
+                    <p>M</p>
+                  </div>
+                  <div className="user grow">
+                    <p>P</p>
+                  </div>
+                  <div className="user grow">
+                    <p>T</p>
+                  </div>
                 </div>
-              </form>
+              </div>
+              <div className="column is-5">
+                <h1 className="title is-4">{this.props.title}</h1>
+                <div className="columns">
+                  <div className="column">
+                    <a className="button is-outlined is-success grow">Share Link</a>
+                  </div>
+                  <div className="column is-8">
+                    <form onSubmit={this.handleSubmit}>
+                      <div className="field has-addons">
+                        <div className="control has-icons-left">
+                            <input value={this.props.dishInput} onChange={this.handleChange} name="dishInput" className="input" placeholder="Enter Dish"/>
+                            <span className="icon is-small is-left">
+                              <i className="fa fa-plus"></i>
+                            </span>
+                          </div>
+                          <div className="control">
+                            <a onClick={this.handleSubmit} className="button is-info">Add</a>
+                          </div>
+                    </div>
+                  </form>
+                  </div>
+                </div>
+              </div>
+              <div className="column is-12">
+                <Dishes dishes={this.props.dishes}/>
               </div>
             </div>
-          </div>
-          <div className="column is-12">
-            <Dishes dishes={dishes}/>
           </div>
         </div>
       </div>
-    </div>
-  </div>
-)}
+    )
+  }
+}
 
 const mapStateToProps = ({session}) => ({dishes: session.dishes, title: session.title, count: session.count, dishInput: session.dishInput})
 
@@ -80,7 +85,6 @@ const mapDispatchToProps = dispatch => bindActionCreators({
   addDishVote,
   addSession,
   setDishInput,
-  changePage: () => push('/1')
 }, dispatch)
 
 export default connect(mapStateToProps, mapDispatchToProps)(Session)
