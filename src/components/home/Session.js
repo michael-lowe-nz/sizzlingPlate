11dd1602abe0b36a8ea5dad89b74866782cc41ad
michael-lowe-nz <mikkeyfluttie@gmail.com>
2018-03-29 17:40:41 +1300

    Styling changes to recents...

--- src/components/home/Session.js
+++ src/components/home/Session.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 export default ({title, id, goToSession}) => {
     return (
-        <div className="column is-12">
-            <button onClick={() => goToSession(id)} className="button is-outlined">{title}</button>
+        <div className="recent-session clickable column is-12">
+            <div onClick={() => goToSession(id)} className=''>
+                <div class="columns">
+                    <h3 className="column is-6 title is-">{title}</h3>
+                    <h3 className="column is-6 has-text-right">{id}</h3>
+                </div>
+            </div>
         </div>
     )
 }
