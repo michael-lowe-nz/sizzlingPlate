d8a614376fc09f27cc43e40a53b8fa2fb04a83ab
michael-lowe-nz <mikkeyfluttie@gmail.com>
2018-03-29 17:42:32 +1300

    Fix class to be className

--- src/components/home/Session.js
+++ src/components/home/Session.js
@@ -4,7 +4,7 @@ export default ({title, id, goToSession}) => {
     return (
         <div className="recent-session clickable column is-12">
             <div onClick={() => goToSession(id)} className=''>
-                <div class="columns">
+                <div className="columns">
                     <h3 className="column is-6 title is-">{title}</h3>
                     <h3 className="column is-6 has-text-right">{id}</h3>
                 </div>
